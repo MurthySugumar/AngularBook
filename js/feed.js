05e368311eeb488ad9ac7e4f3ee01d497bd657c8
MurthySugumar <murthy.sugumar@object-frontier.com>
2014-11-03 14:38:41 +0530

    final build

--- js/feed.js
+++ js/feed.js
@@ -1,84 +1,118 @@
-var feedApp = angular.module('feedApp',['ngRoute','ngCookies','LocalStorageModule']);
-feedApp.config(function($routeProvider) {
+var feedApp = angular.module('feedApp', ['ngRoute', 'ngCookies', 'ngAnimate', 'LocalStorageModule', 'ui.bootstrap']);
+feedApp.config(function ($routeProvider) {
     $routeProvider
-    .when('/',
-        {
-            templateUrl:"feeds.html",
-            controller:"feedController"
+        .when('/', {
+            templateUrl: "feeds.html",
+            controller: "feedController"
         })
-    .when('/profile',
-        {
-            templateUrl:"profile.html",
-            controller:"profileController"
+        .when('/profile', {
+            templateUrl: "profile.html",
+            controller: "profileController"
         })
-    .otherwise({
-        templateUrl: "feed.html"
-      });
+        .otherwise({
+            redirectTo: '/'
+        });;
 });
-feedApp.controller("feedController", function($scope) {
-    $scope.model = {
-        message: "This is my app!!!"
-    }
-});
-/**
- * Services that persists and retrieves TODOs from localStorage
-*/
-feedApp.factory('todoStorage', function () {
-	var STORAGE_ID = 'todos-angularjs-perf';
-
-	return {
-		get: function () {
-			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-		},
-
-		put: function (todos) {
-          //alert("saving:"+JSON.stringify(todos));
-			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-		}
-	};
-});
-feedApp.controller('feedCtrl',function($scope, localStorageService) {
+feedApp.controller('profileController', function ($scope, $modal) {});
+feedApp.controller('feedController', function ($scope, localStorageService) {
 
     $scope.saved = localStorage.getItem('taskItems');
-    $scope.taskItem = (localStorage.getItem('taskItems')!==null) ?
-    JSON.parse($scope.saved) : [ {description: "Why not add a task?", complete: false}];
+    $scope.taskItem = (localStorage.getItem('taskItems') !== null) ?
+        JSON.parse($scope.saved) : [];
     localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
-
     $scope.newTask = "";
+    $scope.validUrl = "";
+    $scope.pattern = new RegExp('^(https?:\\/\\/)?' + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + '(\\?[;&a-z\\d%_.~+=-]*)?' + '(\\#[-a-z\\d_]*)?$', 'i');
 
+    $scope.open = function (size) {
+        var modalInstance = $modal.open({
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+    }
 
     $scope.addNew = function () {
+        $scope.date = new Date();
         if ($scope.newTask == null || $scope.newTask == '') {
-            $scope.taskItem.push({
-
-                description: $scope.newTask
-            })
-            console.log("true"+$scope.newTaskDate);
+            alert("should not be empty");
         } else {
+            if ($scope.pattern.test($scope.newTask)) {
+                $scope.validUrl = "url";
+                $scope.taskItem.push({
+                    description: $scope.newTask,
+                    date: $scope.date,
+                    validUrl: $scope.validUrl
+                })
+            } else {
+                $scope.validUrl = "text";
+                $scope.taskItem.push({
+                    description: $scope.newTask,
+                    date: $scope.date,
+                    validUrl: $scope.validUrl
+                })
+            }
 
-            $scope.taskItem.push({
-                description: $scope.newTask
-            })
-            console.log("false"+$scope.newTask);
         };
         $scope.newTask = '';
-
         localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
     };
+    $scope.redirectUrl = function (url) {
+        if ($scope.pattern.test(url)) {
+            window.open("http://" + url);
+        }
+    }
     $scope.deleteTask = function (index) {
         var completedTask = $scope.taskItem;
         $scope.taskItem = [];
         angular.forEach(completedTask, function (taskItem) {
             if (!taskItem.complete) {
                 $scope.taskItem.push(taskItem);
-
             }
         });
         $scope.taskItem.splice(index, 1);
         localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
     };
+});
 
-    $scope.save = function () {
-        localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
-    }
+feedApp.controller("LogoutDialogController", function ($scope) {
+    $scope.ok = function () {
+        window.location = "feed.html";
+    };
+});
+feedApp.controller('feedModalCtrl', function ($scope, $modal, $log) {
+
+    $scope.open = function (size) {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'feedModal.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                items: function () {}
+            }
+        });
+
+        modalInstance.result.then(function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+});
+
+feedApp.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+    $scope.ok = function () {
+        window.location = "index.html";
+
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
 });
--- js/index.js
+++ js/index.js
@@ -1,17 +1,45 @@
-var indexApp = angular.module('indexApp',[]);
-indexApp.directive('myModal', function() {
+var indexApp = angular.module('indexApp', ['ui.bootstrap']);
+indexApp.directive('myModal', function () {
     return {
-     restrict: 'A',
-     link: function(scope, element, attr) {
-       scope.dismiss = function() {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            scope.dismiss = function () {
 
-       };
-     }
-   }
+            };
+        }
+    }
 });
-indexApp.controller("LoginDialogController",function($scope) {
-    $scope.ok = function(){
-      window.location="feed.html";
+indexApp.controller("LoginDialogController", function ($scope) {
+    $scope.ok = function () {
+        window.location = "feed.html";
+    };
+});
+indexApp.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+    $scope.open = function (size) {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                items: function () {}
+            }
+        });
+
+        modalInstance.result.then(function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
     };
 });
 
+indexApp.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+    $scope.ok = function () {
+        window.location = "feed.html";
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
